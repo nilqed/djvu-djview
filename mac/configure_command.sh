bf5cffd245f85112e2cb1ae6d6bb623f2aa6ce1d
Leon Bottou <leonb@fb.com>
2022-02-24 11:53:44 -0500

    Added macos configure incantation for other pkgs.

--- mac/configure_command.sh
+++ mac/configure_command.sh
@@ -1,13 +1,7 @@
 #/bin/sh
-prefix=/Users/leonb/djvu/INST
-./configure --prefix=${prefix} \
-	--with-extra-includes=${prefix}/include \
-	--with-extra-libraries=${prefix}/lib \
-	--disable-nsdejavu \
-	--enable-mac \
+./configure --prefix=/Users/leonb/djvu/INST \
+	--with-extra-includes=/Users/leonb/djvu/INST/include \
+	--with-extra-libraries=/Users/leonb/djvu/INST/lib \
 	CFLAGS='-arch arm64 -arch x86_64' \
-	OBJCFLAGS='-arch arm64 -arch x86_64' \
-	CXXFLAGS='-arch arm64 -arch x86_64' \
-	QMAKE=${prefix}/bin/qmake \
-	PKG_CONFIG_PATH=${prefix}/lib/pkgconfig
+	CXXFLAGS='-arch arm64 -arch x86_64'
 
